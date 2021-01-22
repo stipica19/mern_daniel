52f55a6463bd795c3ab01f0b25045e68c2e5a790
stipica19 <pipiklepic1@gmail.com>
2021-01-22 21:16:30 +0100

    #progressBarContainer {

--- humac-web/src/components/GalerijaOne.js
+++ humac-web/src/components/GalerijaOne.js
@@ -7,12 +7,19 @@ import "../style.css";
 
 function GalerijaOne() {
   const [selectedImg, setSelectedImg] = useState(null);
+  const handleClick = () => {
+    console.log("aa");
+  };
   return (
     <div>
       <Header />
-      <ImageGrid setSelectedImg={setSelectedImg} />
+      <ImageGrid setSelectedImg={setSelectedImg} onClick={handleClick} />
       {selectedImg && (
-        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
+        <Modal
+          selectedImg={selectedImg}
+          setSelectedImg={setSelectedImg}
+          onClick={handleClick}
+        />
       )}
     </div>
   );
--- humac-web/src/components/Modal.js
+++ humac-web/src/components/Modal.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
+import Header from "./Header.js";
 import "../style.css";
 
 const Modal = ({ setSelectedImg, selectedImg }) => {
+  const [hideHeader, setHideHeader] = useState(false);
   const handleClick = (e) => {
     if (e.target.classList.contains("backdrop")) {
-      console.log("KLIKNULI SMO NA SLIKU");
       setSelectedImg(null);
+      console.log("afafaf");
     }
   };
 
@@ -16,6 +18,7 @@ const Modal = ({ setSelectedImg, selectedImg }) => {
       onClick={handleClick}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
+      style={{ marginTop: "10px" }}
     >
       <motion.img
         src={selectedImg}
